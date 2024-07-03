7c3a3cfa319caaf5ed3d57b030272a3f4f49ee26
Henok-Haile <henokhaile53@gmail.com>
2024-07-03 03:24:41 +0000

    Task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@
  * Joins a set of strings with a dash after stripping the strings of
  * a leading sub string.
  * param {Set<String>} set - A collection of strings.
- * param {String} startString - The string to strip 
+ * param {String} startString - The string to strip
  * from the beginning of each item in the set.
  * returns {String}
  */
